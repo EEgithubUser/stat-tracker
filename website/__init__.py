7583eac880d4e4bf39336524bca9be107fb6e81e
Kevin <kevchan129@gmail.com>
2023-03-07 21:40:38 -0800

    database file(database.db): generated fresh database.db file and fresh migrations folder. Use database migrations with Alembic to create db, replacing SQLAlchemy

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,17 +14,17 @@ def create_app():
     app.config['SECRET_KEY'] = '333673015814151940945230455237845615952'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     from .views import views
     from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
     
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -35,7 +37,7 @@ def create_app():
     return app
 
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
