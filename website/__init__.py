3234de656d353de16a7a4fb794f04f1906c7cc2d
Kevin <kevchan129@gmail.com>
2023-03-09 01:41:24 -0800

    Implement Postgresql with environmental variables

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,22 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config['SECRET_KEY'] = '333673015814151940945230455237845615952'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     db.init_app(app)
-    migrate = Migrate(app, db)
+    migrate = Migrate(app, db, compare_type=True)
 
     from .views import views
     from .auth import auth
